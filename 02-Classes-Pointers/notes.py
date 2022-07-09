e8e9caa126fe7840ab5f77d880ed617d7ff6abbe
ohimjosh <joshmiranda.exe@gmail.com>
2022-07-09 18:06:21 -0400

    Pointer notes

--- 02-Classes-Pointers/notes.py
+++ 02-Classes-Pointers/notes.py
@@ -29,11 +29,59 @@ cookie_one = Cookie('green')
 
 cookie_two = Cookie('blue')
 
-print('Cookie one is', cookie_one.get_color())
-print('Cookie two is', cookie_two.get_color())
+# print('Cookie one is', cookie_one.get_color())
+# print('Cookie two is', cookie_two.get_color())
 
 cookie_one.set_color('yellow')
-print('Cookie one is now', cookie_one.get_color())
-print('Cookie two is still', cookie_two.get_color())
+# print('Cookie one is now', cookie_one.get_color())
+# print('Cookie two is still', cookie_two.get_color())
 
 # -----------------------------------------------------------
+
+# What is a pointer?
+# holds memory address of a value
+# variable will point to the memory address giving it the value
+
+dict1 = {
+        'value': 11
+        }
+
+dict2 = dict1
+
+print("Before value is updated:")
+print("dict1 =", dict1)
+print("dict2 =", dict2)
+
+dict1['value'] = 22
+
+print("\nAfter value is updated:")
+print("dict1 =", dict1)
+print("dict2 =", dict2)
+
+# they both point to the same value so the value is now 22
+
+# pointers can be redirected
+
+dict3 = {
+        'value': 57
+        }
+
+dict2 = dict3
+
+print("\nAfter pointer is redirected:")
+print("dict1 =", dict1)
+print("dict2 =", dict2)
+print("dict3 =", dict3)
+
+# dict2 is now pointing to dict3
+
+# What happens when you point dict1 to dict3
+dict1 = dict3
+
+# since dict1 is now 57 it the value of 22 will go through garbage collection
+# garbage collection clears up unused memory 
+
+print("\nPointers all point dict3:")
+print("dict1 =", dict1)
+print("dict2 =", dict2)
+print("dict3 =", dict3)
