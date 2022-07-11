9165158c13c029f56d62bf86abb904cc0b906c7f
ohimjosh <joshmiranda.exe@gmail.com>
2022-07-10 20:37:45 -0400

    list vs linked list

---
+++ 03-Linked-List/notes.py
@@ -0,0 +1,12 @@
+# -----------------------------------------------------------
+
+# What is the difference b/w a list and linked list?
+
+# linked list do not have indexes
+# linked list nodes are spread all over the place
+# there is a head, that points to the first node 
+# and there is a tail, points to the last node
+# each node points to the next to the next to the next etc 
+# the last will point to none
+
+# -----------------------------------------------------------
