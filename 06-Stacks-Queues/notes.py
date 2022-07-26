dfb84382720e4354457c5e658fcd92094277a259
ohimjosh <joshmiranda.exe@gmail.com>
2022-07-26 10:37:21 -0400

    stacks & queues

---
+++ 06-Stacks-Queues/notes.py
@@ -0,0 +1,25 @@
+# -----------------------------------------------------------
+# Stacks & Queues
+
+# LIFO
+# Last In First Out
+
+# lets say you have a tube container of tennis balls
+# you place one in and then a second one
+# the only tennis ball that can be taken out is the last one 
+# if you have three tennis balls in the tube
+# you dont have access to the second one anymore 
+# you can only take out the third ball
+
+# lets say we have a list 
+# 11 3 23 7
+#  0 1  2 3
+
+# if we were to add a item at the end of that list
+# Big O: O(1)
+
+# BUT if we were to add an item to the front
+# Big O: O(n)
+# since we will need to go through the list 
+
+# -----------------------------------------------------------
