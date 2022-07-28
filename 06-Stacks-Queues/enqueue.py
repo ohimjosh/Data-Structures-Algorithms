c6d961dd26a95f9c4aae9a3b00925494f4c5ca2c
ohimjosh <joshmiranda.exe@gmail.com>
2022-07-28 14:26:23 -0400

    enqueue

---
+++ 06-Stacks-Queues/enqueue.py
@@ -0,0 +1,46 @@
+# -----------------------------------------------------------
+
+# Enqueue
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def enqueue(self, value):
+        new_node = Node(value)
+
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+
+        else:
+            # the last pointer will point to new node
+            self.last.next = new_node
+
+            # last node is now the new node
+            self.last = new_node
+
+        self.length += 1
+        return True
+
+my_queue = Queue(1)
+my_queue.enqueue(2)
+
+my_queue.print_queue()
+
+# -----------------------------------------------------------
+        
