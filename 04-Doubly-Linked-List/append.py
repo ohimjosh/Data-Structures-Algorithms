0e5abea0cbd44e1c995a45b7925a459b99a461c2
ohimjosh <joshmiranda.exe@gmail.com>
2022-09-12 16:00:48 -0400

    append

---
+++ 04-Doubly-Linked-List/append.py
@@ -0,0 +1,43 @@
+# -----------------------------------------------------------
+
+# Append 
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+
+        # if linked list is empty 
+        if self.head is not None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+        self.length += 1
+        return True
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
+my_doubly_linked_list.append(3)
+
+my_doubly_linked_list.print_list()
